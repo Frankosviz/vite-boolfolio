195d8d41eb89cd1eeaa4954df3a0459505c1c6cc
Frankosviz <geom.francescopaolo@gmail.com>
2024-06-13 12:38:23 +0200

    Contact form

--- src/components/ContactComponent.vue
+++ src/components/ContactComponent.vue
@@ -2,9 +2,21 @@
     <div>
         Contact Us
         <div class="row">
+            <div class="col-12">
+                <div class="alert alert-success" v-if="success">
+                    Messaggio inviato con successo!
+                </div>
+            </div>
+        </div>
+        <div v-if="errors">
+            <ul>
+                <li v-for="error in errors">{{ error }}</li>
+            </ul>
+        </div>
+        <div class="row">
             <form @submit.prevent="sendForm()" class="col-12 text-start">
                 <div class="mb-3">
-                    <input type="text" class="form-control border-0 border-bottom" placeholder="Name" v-model="name">
+                    <input type="text" :class="{ 'is-invalid': errors.name }" class="form-control border-0 border-bottom" placeholder="Name" v-model="name">
                 </div>
                 <div class="mb-3">
                     <input type="email" class="form-control border-0 border-bottom" placeholder="Email" v-model="email">
@@ -13,7 +25,7 @@
                     <label for="message">Your message</label>
                     <textarea id="message" cols="30" rows="10" class="form-control border-0 border-bottom" placeholder="Message" v-model="message">{{ message }}</textarea>
                 </div>
-                <button type="submit" class="btn btn-primary">Send</button>
+                <button type="submit" :disabled="loading" class="btn btn-primary">{{ loading ? 'Sending...' : 'Send' }}</button>
             </form>
         </div>
     </div>
@@ -29,11 +41,16 @@ export default {
             store,
             name: '',
             email: '',
-            message: ''
+            message: '',
+            errors: {},
+            loading: false,
+            success: false
         }
     },
     methods: {
         sendForm() {
+            this.success = false;
+            this.loading = true;
             const data ={
                 name: this.name,
                 address: this.email,
@@ -42,10 +59,16 @@ export default {
             // console.log(data)
             axios.post(`this.store.apiBaseUrl + /contacts`, data).then((res) => {
                 console.log(res.data)
-            }).catch((err) => {
-                console.log(err)
+                this.success = true;
+                this.name = '';
+                this.email = '';
+                this.message = '';
+            }).catch((error) => {
+                this.errors = error.response.data.errors;
             }).finally(() => {
+                // Nella finally svuotiamo i campi dopo che l'utente ha inviato il form, solo se tutto è andato bene
                 
+                this.loading = false;
             })
         }
     }
